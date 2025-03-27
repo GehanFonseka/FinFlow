23004d2c5e7cbae75dd82150097378b169e97c1f
kavishka <kavishkadeshan@428gmail.com>
2025-03-27 11:38:31 +0530

    final23

--- Finance-Tracker-Frontend/src/routes/router.jsx
+++ Finance-Tracker-Frontend/src/routes/router.jsx
@@ -14,6 +14,7 @@ import AddGoal from "../pages/goal/AddGoal";
 import Expense from "../pages/expense/Expense";
 import AddExpense from "../pages/expense/AddExpense";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
