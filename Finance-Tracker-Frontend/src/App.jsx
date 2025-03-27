4d3e46a1bcded6612eb57e4d376e2d73e3859668
kavishka <kavishkadeshan@428gmail.com>
2025-03-27 11:43:28 +0530

    final26

--- Finance-Tracker-Frontend/src/App.jsx
+++ Finance-Tracker-Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -32,6 +33,7 @@ function App() {
     </>
   );
 }
+
 
 
 export default App;
