355c6b9e57fdbd880d89d0a636012fa8754bd0b1
GehanFonseka <gehanfonseka@gmail.com>
2025-03-29 23:54:11 +0530

    final

--- Finance-Tracker-Frontend/axios-client.jsx
+++ Finance-Tracker-Frontend/axios-client.jsx
@@ -4,7 +4,7 @@ const axiosClient = axios.create({
   baseURL: `http://localhost:5000/api`,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*", // Allow requests from all origins (change this as needed)
+    "Access-Control-Allow-Origin": "*", 
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE", // Allow specific HTTP methods
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, Content-Type, Accept", // Allow specific headers
