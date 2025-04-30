350334ccbd5aaf1baad5646be664696c0c7a6532
ChamuHimasha <chamuhimasha29471@gmail.com>
2025-04-30 19:45:46 +0530

    edex

--- Finance-Tracker-Frontend/src/pages/expense/EditExpense.jsx
+++ Finance-Tracker-Frontend/src/pages/expense/EditExpense.jsx
@@ -116,6 +116,7 @@ const EditExpense = ({ isOpen, onClose, fetchExpense, selectedExpenseId }) => {
     }  else if (/^[^a-zA-Z]/.test(editedExpense.title.trim())) {
       newErrors.title = "Expense Title must not start with a number or special character";
     }
+    
 
     // Validate Description
     if (!editedExpense.description.trim()) {
