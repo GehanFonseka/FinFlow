6d8a2cd9af45efb2f030e03e05f942e1466d21e8
kavishka <kavishkadeshan@428gmail.com>
2025-04-30 18:49:35 +0530

    commit 4

--- Finance-Tracker-Backend/controllers/dashBoardController.js
+++ Finance-Tracker-Backend/controllers/dashBoardController.js
@@ -57,6 +57,7 @@ exports.getData = async (req, res) => {
       allExpenses,
       pendingGoals,
       completeGoals,
+      
     });
   } catch (err) {
     console.error("Get Budgets Error:", err);
