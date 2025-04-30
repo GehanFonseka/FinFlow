1ab17e32455df6a764efa8341763c6c43efef01b
kavishka <kavishkadeshan@428gmail.com>
2025-04-30 18:44:20 +0530

    commit 1

--- Finance-Tracker-Backend/models/budgetModel.js
+++ Finance-Tracker-Backend/models/budgetModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+
+
 const budgetSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   budgetName: { type: String, required: true },
